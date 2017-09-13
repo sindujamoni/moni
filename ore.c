47e0a5fb85f1ffa0de5d7d7c95fcd75aae7ef5ec
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2017-09-13 14:45:54 +0530

    Create ore.c

---
+++ ore.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("%d is a even number",a);
+}
+else
+{
+printf("%d is a odd number",a);
+}
+return o;
+}
